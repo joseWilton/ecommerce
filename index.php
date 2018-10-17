95f935ef5732ebe469c245ebf192c680ed1b489d
Jose Wilton <josewilltton@gmail.com>
2018-10-17 19:37:10 -0300

    criando o template admin e definindo sua rota

--- index.php
+++ index.php
@@ -4,17 +4,26 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
 
 $app = new Slim();
 
 $app->config('debug', true);
 
-$app->get('/', function() {
+$app->get('/', function() { //rota para o site
 	
 	$page = new Page();
 	$page->setTpl("index");	
 
 });
+
+$app->get('/admin', function() { //rota para a área de administração
+	
+	$page = new PageAdmin();
+	$page->setTpl("index");	
+
+});
+
 
 $app->run();
 
